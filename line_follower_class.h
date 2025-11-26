e4f070c96975663a953d3c8f7e28ab101273ef4a
Niño James Tan <njtan142@gmail.com>
2025-11-27 04:19:03 +0800

    150 fine

--- line_follower_class.h
+++ line_follower_class.h
@@ -28,7 +28,7 @@ private:
     int linePosition;
     int lastKnownPosition;
     bool lastButtonState;
-    int baseSpeed = 140;
+    int baseSpeed = 150;
     int turnSpeed = 110;
     int backwardBias = -18;
     unsigned long turnTimeout = 550;
